98854b7c176f9ac7affd20bea5d185b02924b48a
okikukawa <71206682+okikukawa@users.noreply.github.com>
2020-10-13 15:43:05 +0900

    Update exam.rb

--- exam.rb
+++ exam.rb
@@ -7,7 +7,7 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/exam', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
 server.mount('/expect_myself.cgi', WEBrick::HTTPServlet::CGIHandler, 'expect_myself.rb')
 server.mount('/bad.cgi', WEBrick::HTTPServlet::CGIHandler, 'bad.rb')
 server.start
